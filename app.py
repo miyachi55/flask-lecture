c13839ab35e795b6dd566c1f7287f8a2fde22ad4
miyajiaki <miyachi.com009500@gmail.com>
2020-07-28 20:42:42 +0900

    テンプレートまでしたよ

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,7 +7,25 @@ def hello():
 
 @app.route('/top')
 def top():
-    return"ここはtopだよ( ^ω^ )"
+    return"なにもっと見てんだよ( ^ω^ )"
+
+@app.route('/hello/<text>')
+def name(text):
+    return text +"さん、こんにちは( ´∀｀)"
+
+@app.route('/index')
+def index():
+    name="Zxys"
+    age = 25
+    adress ="高松市"
+    return render_template('index.html',user_name = name,user_adress = adress,user_age = age)
+
+@app.route('/weather')
+def weather():
+    ten="晴れ"
+    return render_template('weather.html',today =ten)
+    var = input('入力してに')
+
 
 
 if __name__ == '__main__':
