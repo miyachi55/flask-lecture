5ccad934997791b9229144fca64162be96aeef0e
miyajiaki <miyachi.com009500@gmail.com>
2020-07-29 20:32:53 +0900

    404までできたよ

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
 import sqlite3
 app = Flask(__name__)
 
@@ -43,7 +43,32 @@ def dbtest():
 @app.route('/add')
 def add_get():
     return render_template('add.html')
-    
+
+@app.route('/add',methods=['post'])
+def app_post():
+    py_task = request.form.get("task")
+    conn = sqlite3.connect('flasktest.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO task VALUES (null,?)",(py_task,))
+    conn.commit()
+    conn.close()
+    return redirect('/list')
+
+@app.route('/list')
+def task_list():
+    conn = sqlite3.connect('flasktest.db')
+    c = conn.cursor()
+    c.execute("SELECT id, task FROM task")
+    task_list_py =[]
+    for row in c.fetchall():
+        task_list_py.append({"id":row[0],"task":row[1]})
+    c.close()
+    print(task_list_py)
+    return render_template("tasklist.html",task_list = task_list_py)
+
+@app.errorhandler(404)
+def notfound(code):
+    return "404エラーだお_:(´ཀ`」 ∠):"
 
 
 
